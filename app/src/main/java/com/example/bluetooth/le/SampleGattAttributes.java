eb8a3b26cd42b9ef2fde51ea4e4967e861d4a17d
mattellegrino <s267652@studenti.polito.it>
2022-05-19 15:44:11 +0200

    Add string Realtime Steps

--- app/src/main/java/com/example/bluetooth/le/SampleGattAttributes.java
+++ app/src/main/java/com/example/bluetooth/le/SampleGattAttributes.java
@@ -25,6 +25,7 @@ public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
     public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+    public static String STEPS = "00000007-0000-3512-2118-0009af100700";
 
     static {
         // Sample Services.
@@ -32,6 +33,7 @@ public class SampleGattAttributes {
         attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
         // Sample Characteristics.
         attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+        attributes.put(STEPS, "Realtime Steps");
         attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
     }
 
